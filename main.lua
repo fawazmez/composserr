51fabde57863414c1951979a54b3c5386b7b2793
fawazmez <36240699+fawazmez@users.noreply.github.com>
2018-05-15 14:46:15 -0400

    Initial commit

---
+++ main.lua
@@ -0,0 +1,13 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+-- composser
+-- By fawaz mezher
+-- on may 2018
+-----------------------------------------------------------------------------------------
+
+local composer = require( "composer" )
+
+display.setStatusBar(display.HiddenStatusBar)
+
+composer.gotoScene( "scene1" )
